4985d909682a2f236142fbb00926d86ece53da60
Eit-zw <zhuwei471919@163.com>
2017-06-28 11:05:28 +0800

    xiugai

--- gongyishu/js/main.js
+++
@@ -1,3 +0,0 @@
-(function(a,d){var b=a.documentElement,c=function(){b.style.fontSize=b.clientWidth/6.4+"px"};d.addEventListener("resize",c,!1);a.addEventListener("DOMContentLoaded",c,!1)})(document,window);
-$(function(){})
-   
